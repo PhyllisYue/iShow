b491fdfce916520491d3e5d3a4741d8743dff8bf
PhyllisYue <285794144@qq.com>
2016-06-02 10:54:21 +0800

    Initial Commit

---
+++ iShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iShow
+//
+//  Created by Phyllis on 16/6/2.
+//  Copyright © 2016年 Phyllis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iShow
+//
+//  Created by Phyllis on 16/6/2.
+//  Copyright © 2016年 Phyllis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
