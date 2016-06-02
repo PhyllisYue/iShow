9c61ee506c3c28f9b4f8e8137bc10cc8e078c10f
PhyllisYue <285794144@qq.com>
2016-06-02 14:29:39 +0800

    框架第三次修改

--- iShow/所需文件/Common/Header.h
+++ iShow/所需文件/Common/Header.h
@@ -1,3 +1,4 @@
+
 //
 //  Header.h
 //  iShow
---
+++ iShow/所需文件/Common/JsonParser.m
@@ -0,0 +1,27 @@
+//
+//  JsonParser.m
+//  PRO1
+//
+//  Created by CORYIL on 16/3/25.
+//  Copyright © 2016年 徐锐. All rights reserved.
+//
+
+#import "JsonParser.h"
+
+@implementation JsonParser
+
++ (id)jsonFromFileName:(NSString *)fileName{
+
+    //1.文件路径
+    NSString *path = [[NSBundle mainBundle]pathForResource:fileName ofType:@"json"];
+    
+    //2.文件 -> data
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    
+    //3.从数据中读取字典或者数组
+    id json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+    
+    return json;
+}
+
+@end
