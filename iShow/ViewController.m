5cd48d003df1d76f2e0ec423f7203281656ecb36
PhyllisYue <285794144@qq.com>
2016-06-02 11:12:50 +0800

    1
    1

--- iShow/ViewController.m
+++ iShow/ViewController.m
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    /**
+     *  <#Description#>
+     *
+     *  @return <#return value description#>
+     */
 }
 
 - (void)didReceiveMemoryWarning {
